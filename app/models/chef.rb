1ad0cce3cc611edbbbba2b539aa7431eb6b86272
Hari Bellam <hkworkout@gmail.com>
2017-05-27 09:30:58 -0500

    create chefs and recipes associations

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,3 +1,9 @@
 class Chef <ApplicationRecord
   has_many :recipes
+  before_save { self.email = email.downcase }
+  validates :chefname, presence: true, length: { minimum: 5, maximum: 40 }
+  VALID_REG_EXPR = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 105 },
+              uniqueness: { case_sensitive: false }, 
+              format: { with: VALID_REG_EXPR }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170516213535) do
+ActiveRecord::Schema.define(version: 20170527134430) do
 
   create_table "chefs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "chefname"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170516213535) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "chef_id"
   end
 
 end
