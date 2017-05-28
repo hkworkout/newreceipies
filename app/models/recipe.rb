5f366b332939a77d074fa278d5473c2d7816ff15
Hari Bellam <hkworkout@gmail.com>
2017-05-27 20:59:37 -0500

    new & edit forms and partials

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe <ApplicationRecord
   belongs_to :chef, dependent: :destroy
   validates :name, presence: true, length: { minimum: 3, maximum: 40}
-  validates :description, presence: true, length: { minimum: 3, maximum: 40}
-  validates :summary, presence: true, length: { minimum: 3, maximum: 40}
+  validates :description, presence: true, length: { minimum: 3, maximum: 250}
+  validates :summary, presence: true, length: { minimum: 3, maximum: 100}
   validates :chef_id, presence: true
 end
